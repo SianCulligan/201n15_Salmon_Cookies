fb039439daeedd40af56e2ef541388e16f677447
Sian Culligan <SianCulligan@gmail.com>
2019-08-24 14:36:36 -0700

    added basic language for each file

---
+++ js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// TODO: Create 5 Object literals
+// You have starting data, and a goal of end data that includes both arrays and single values. Think about what arrays you might need and also how you will use object methods to do your calculations. Keep in mind the single responsibility principle: a function/method should do only one thing.
+
+//  Good use of Object Literals (no constructors allowed today); one for each store model; properties/values and methods are correctly constructed and given meaningful names
+
+// Main page meets requirements of the problem domain
+
+// Use template literals in your JS logic to render the stores as lists on the sales page
+
+//create the element
+var storeListEl = document.getElementById('store_list')
+
+
+// create 'stores' as an object literal and then each location can live inside 'stores'
